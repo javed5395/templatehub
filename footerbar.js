dfc0b18c20e51413110fe651be213b17a7ccb4bf
javed5395 <javed5395@gmail.com>
2026-07-31 19:34:50 +0500

    Fix broken X link: lazydogtemplates -> lazydogtemplate (correct handle)

--- footerbar.js
+++ footerbar.js
@@ -91,7 +91,7 @@
 
   <div class="footer-mid">
     <div class="footer-social">
-      <a href="https://x.com/lazydogtemplates" title="X (Twitter)" class="ic-x" target="_blank" rel="noopener">
+      <a href="https://x.com/lazydogtemplate" title="X (Twitter)" class="ic-x" target="_blank" rel="noopener">
         <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-4.714-6.231-5.401 6.231H2.744l7.73-8.835L1.254 2.25H8.08l4.253 5.622zm-1.161 17.52h1.833L7.084 4.126H5.117z"/></svg>
       </a>
       <a href="https://www.instagram.com/lazydogtemplates/" title="Instagram" class="ic-ig" target="_blank" rel="noopener">
